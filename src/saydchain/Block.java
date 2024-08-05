0777441315bf3511ccf2309c99e50de9075e9397
Ayden Smith <s_ayden@ymail.com>
2024-08-05 14:05:02 +1000

    Added mineBlock method

--- src/saydchain/Block.java
+++ src/saydchain/Block.java
@@ -7,8 +7,10 @@ public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //our data will be a simple message.
-    private final long timeStamp; //Timestamp of block creation
+    private String data; // Data of block.
+    private final long timeStamp; // Timestamp of block creation
+    private int nonce; //Random number used in mining
+    public static int mineDifficulty = 5; //Difficulty of mining
 
     //Block Constructor.
     public Block(String data, String previousHash) {
@@ -18,7 +20,27 @@ public class Block {
         this.hash = calculateHash();
     }
 
+    /**
+     * Calculates the hash for the block.
+     * <p>
+     * This method uses the SHA-256 algorithm to generate a hash based on the
+     * previous block's hash, the current block's timestamp, and the block's data.
+     *
+     * @return The calculated hash as a String.
+     */
     public String calculateHash() {
-        return StrUtil.appSha256(previousHash + Long.toString(timeStamp) + data);
+        return StrUtil.appSha256(previousHash + Long.toString(nonce) + data);
+    }
+
+    public void mineBlock(int mineDifficulty) {
+        String target = new String(new char[mineDifficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+
+        // Keep calculating the hash until it starts with the target string
+        while (!hash.substring(0, mineDifficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+
+        System.out.println("Mined block: " + hash);
     }
 }
